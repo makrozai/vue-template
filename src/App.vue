a433e112e6c0738e06772b195039db6aaf9a2e10
Marco <marco.condoric@gmail.com>
2018-09-11 17:46:17 -0500

    agrega ejemplo con estructura vuex y router

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,54 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='@/assets/logo.png')
     h1 {{ msg }}
     h2 Essential Links
-    ul
-      li
-        a(href='https://vuejs.org', target='_blank') Core Docs
-      li
-        a(href='https://forum.vuejs.org', target='_blank') Forum
-      li
-        a(href='https://chat.vuejs.org', target='_blank') Community Chat
-      li
-        a(href='https://twitter.com/vuejs', target='_blank') Twitter
-    h2 Ecosystem
-    ul
-      li
-        a(href='http://router.vuejs.org/', target='_blank') vue-router
-      li
-        a(href='http://vuex.vuejs.org/', target='_blank') vuex
-      li
-        a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
-      li
-        a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
+    div
+      h3 contador
+      p {{ count }}
+      p {{ getDouble }}
+
+      div
+        button(@click="increment") +
+        button(@click="decrement") -
+      div
+        button(@click="increment10") +10
+        button(@click="incrementAsync") +Async
 
 </template>
 
 <script>
+import { mapState, mapMutations, mapGetters } from 'vuex'
 export default {
   name: 'app',
+
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  computed: {
+    ...mapState(['count']),
+
+    ...mapGetters(['getDouble'])
+  },
+
+  methods: {
+    ...mapMutations(['increment', 'decrement']),
+    increment10() {
+      this.$store.commit('increment', {
+        number: 10
+      })
+    },
+
+    incrementAsync(){
+      this.$store.dispatch('incrementAsync',{
+        number: 2
+      })
+      .then(() => {
+        console.log('action terminada')
+      })
     }
   }
 }
